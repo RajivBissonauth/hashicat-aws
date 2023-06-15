bd65834ee0fbef2b30250ebd572068e8ccd9bd47
Rajiv Bissonauth <rajiv.bissonauth@gmail.com>
2023-06-15 21:25:12 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -120,6 +120,7 @@ resource "aws_eip_association" "hashicat" {
 
 resource "aws_instance" "hashicat" {
   ami                         = data.aws_ami.ubuntu.id
+  department                  = devops
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.hashicat.key_name
   associate_public_ip_address = true
